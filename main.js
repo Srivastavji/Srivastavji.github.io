48a8c965a9a44146f985724614255eb1c1319183
Aman Srivastav <amansrivastava3516@gmail.com>
2022-04-02 12:26:47 +0530

    Update Resume Section

--- main.js
+++ main.js
@@ -6,22 +6,3 @@ function navtoogle() {
             x.style.display='grid'
     }
 }
-
-function sendEmail(){
-        Email.send({
-                Host :"smtp.gmail.com",
-                Username : "amansrivastava3516@gmail.com",
-                Password : "Sriv@stavji#@1609#",
-                To : 'amansrivastava3516@gmail.com',
-                From : document.getElementById("email").value,
-                Subject : 'Message from Website',
-                Body : "And this is Body tag",
-                
-                // "Name: " + document.getElementById("name").value
-                //       +"<br> Email : " + document.getElementById("email").value
-                //       +"<br> Message : " + document.getElementById("message").value
-                     
-        }).then(
-               message => alert("Submitted Successfully")
-             );   
-   }
